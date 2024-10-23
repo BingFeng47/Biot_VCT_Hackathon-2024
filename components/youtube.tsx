4701e94a97656020369784a8284136daca4b76ba
Bing Feng Lim <104261673+BingFeng47@users.noreply.github.com>
2024-10-23 16:30:07 +0800

    Update youtube.tsx

--- components/youtube.tsx
+++ components/youtube.tsx
@@ -3,17 +3,17 @@ import HeroVideoDialog from "./ui/hero-video-dialog";
 export function Youtube() {
   return (
     <div className="">
-      <HeroVideoDialog
+     <HeroVideoDialog
         className="dark:hidden block"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/watch?v=PQJ-bKCpuZk"
+        videoSrc="https://www.youtube.com/embed/PQJ-bKCpuZk"
         thumbnailSrc="/docs/hero.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/watch?v=PQJ-bKCpuZk"
+        videoSrc="https://www.youtube.com/embed/PQJ-bKCpuZk"
         thumbnailSrc="/docs/hero.png"
         thumbnailAlt="Hero Video"
       />
