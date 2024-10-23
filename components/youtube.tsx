8f93ccdf88ff0bfa72251cc9f36a598240d9f015
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2024-10-23 16:28:02 +0800

    fix overlay youtube link

--- components/youtube.tsx
+++ components/youtube.tsx
@@ -2,18 +2,18 @@ import HeroVideoDialog from "./ui/hero-video-dialog";
 
 export function Youtube() {
   return (
-    <div className="">
+    <div className="relative">
       <HeroVideoDialog
         className="dark:hidden block"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/watch?v=PQJ-bKCpuZk"
+        videoSrc="https://www.youtube.com/embed/PQJ-bKCpuZk"
         thumbnailSrc="/docs/hero.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://www.youtube.com/watch?v=PQJ-bKCpuZk"
+        videoSrc="https://www.youtube.com/embed/PQJ-bKCpuZk"
         thumbnailSrc="/docs/hero.png"
         thumbnailAlt="Hero Video"
       />
