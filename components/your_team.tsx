ac2d940abe95ec9e0700446b8ee605ee641162f3
Bing <145952666+This-Is-Bing@users.noreply.github.com>
2024-10-24 23:01:54 +0800

    remove players images and team logo

--- components/your_team.tsx
+++ components/your_team.tsx
@@ -41,7 +41,7 @@ function YourTeam({id,tier,image,player_name, first_name, last_name, team, acron
             <div className='flex flex-row items-center gap-2'>
                 <div>
                 <div className='bg-accent rounded-full'>
-                <Image src={'/placeholder/placholder_val.png'} alt="logo" width={40} height={40} className='p-1 object-center'/>
+                <Image src={'/placeholder/placeholder_val.png'} alt="logo" width={40} height={40} className='p-1 object-center'/>
 
                   {/* {
                 light_logo_url?
